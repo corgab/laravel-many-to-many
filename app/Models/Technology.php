e9448ce3012f77f47fa71b26a9cb897b088eb1bc
corgab <corbanigabriele@gmail.com>
2024-06-07 15:41:00 +0200

    ADD: project technology migration

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Technology extends Model
 {
     use HasFactory;
+
+    public function projects() {
+
+        return $this->belongsToMany('App\Models\Project');
+    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TypeSeeder::class,
+            Technology::class,
             UserSeeder::class,
             ProjectSeeder::class
         ]);
